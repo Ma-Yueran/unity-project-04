4504790c5e421e1d0f0a5ffa155caa52889eb062
Ma-Yueran <mayuerantom@outlook.com>
2020-12-30 17:25:20 +0800

    Add player motion and player controls scripts

---
+++ UnityProject04/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MYR
+{
+    public class PlayerControls : MonoBehaviour
+    {
+        public Transform playerCamera;
+
+        public float inputH;
+        public float inputV;
+        public bool isMoving;
+
+        private void Update()
+        {
+            inputH = Input.GetAxis("Horizontal");
+            inputV = Input.GetAxis("Vertical");
+            isMoving = inputH != 0 || inputV != 0;
+        }
+
+        public Vector3 GetTargetDirection()
+        {
+            Vector3 targetDirection = playerCamera.transform.forward * inputV;
+            targetDirection += playerCamera.transform.right * inputH;
+            targetDirection.y = 0;
+            targetDirection.Normalize();
+
+            return targetDirection;
+        }
+    }
+}
---
+++ UnityProject04/Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MYR
+{
+    public class PlayerMotion : MonoBehaviour
+    {
+        public float speed = 10;
+
+        private Transform myTransform;
+        private new Rigidbody rigidbody;
+        private PlayerControls playerControls;
+
+        private void Awake()
+        {
+            myTransform = transform;
+            rigidbody = GetComponent<Rigidbody>();
+            playerControls = GetComponent<PlayerControls>();
+        }
+
+        private void Update()
+        {
+            HandleMovement();
+            HandleRotation();
+        }
+
+        private void HandleMovement()
+        {
+            if (!playerControls.isMoving)
+            {
+                rigidbody.velocity = Vector3.Lerp(rigidbody.velocity, Vector3.zero, 0.1f);
+                return;
+            }
+
+            myTransform.position += playerControls.GetTargetDirection() * speed * Time.deltaTime;
+        }
+
+        private void HandleRotation()
+        {
+            if (!playerControls.isMoving)
+            {
+                return;
+            }
+
+            Quaternion targetDirection = Quaternion.LookRotation(playerControls.GetTargetDirection());
+            myTransform.rotation = Quaternion.Lerp(myTransform.rotation, targetDirection, 0.1f);
+        }
+    }
+}
