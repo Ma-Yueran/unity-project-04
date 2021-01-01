4e16e92d6f6caa8aefdbba40cdb05514a23f0360
Ma-Yueran <mayuerantom@outlook.com>
2021-01-02 00:15:38 +0800

    Add ResetAnimatorBool script

---
+++ UnityProject04/Assets/Scripts/ResetAnimatorBool.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetAnimatorBool : StateMachineBehaviour
+{
+    public string boolName;
+    public bool value;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(boolName, value);
+    }
+}
